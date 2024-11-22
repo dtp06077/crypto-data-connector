295e1b8ce488d6c5fe535da850b45a7b59433968
dtp06077 <dtp06077@gmail.com>
2024-11-22 15:56:01 +0900

    INIT COMMIT

---
+++ src/main/java/com/example/cryptodataconnector/CryptoDataConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.cryptodataconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoDataConnectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoDataConnectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cryptodataconnector/CryptoDataConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cryptodataconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoDataConnectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
