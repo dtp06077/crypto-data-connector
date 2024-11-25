b73643b4bfbdc5342291b786d056cbb0573df3ed
dtp06077 <dtp06077@gmail.com>
2024-11-25 14:58:50 +0900

    FEAT: 영속성 컨텍스트 현재가 저장 메서드 추가

--- src/main/java/com/example/cryptodataconnector/repository/TickerRepository.java
+++ src/main/java/com/example/cryptodataconnector/repository/TickerRepository.java
@@ -1,7 +1,19 @@
 package com.example.cryptodataconnector.repository;
 
+import com.example.cryptodataconnector.domain.Ticker;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class TickerRepository {
+
+    private final EntityManager em;
+
+    //현재가 저장
+    public long save(Ticker ticker) {
+        em.persist(ticker);
+        return ticker.getId();
+    }
 }
